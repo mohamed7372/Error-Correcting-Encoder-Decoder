7591942059149819130a0de1f55c615436883d2a
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-08-01 19:48:36 +0200

    update2
    correct erros in message and decoding

--- src/errorCorrectingEncoderDecoder/ErrorCorrectingEncoderDecoder.java
+++ src/errorCorrectingEncoderDecoder/ErrorCorrectingEncoderDecoder.java
@@ -9,9 +9,17 @@ public class ErrorCorrectingEncoderDecoder {
 	static final Random random = new Random();
 	
 	public static void main(String[] args) {
-		String input = sc.nextLine();
-		String output = createError(input);
-		System.out.println(output);
+		String message = sc.nextLine();
+		System.out.println(message);
+		// encode message
+		String msgSend = encodeMsg(message);
+		System.out.println(msgSend);
+		// msg with errors
+		msgSend = createError(msgSend);
+		System.out.println(msgSend);
+		// decode msg + correction
+		String msgCorrect = decodeMsg(msgSend);
+		System.out.println(msgCorrect);
 	}
 
 	static String createError(String input) {
@@ -45,5 +53,36 @@ public class ErrorCorrectingEncoderDecoder {
 		int posErr = random.nextInt(3);
 		arr[posErr] = randomChar();
 		return new String(arr);
+	}
+	
+	static String encodeMsg(String msg) {
+		String s = "";
+		int j;
+		for (int i = 0; i < msg.length(); i++) {
+			j = 1;
+			while(j <= 3) {
+				s += msg.charAt(i);
+				j++;
+			}
+		}
+		return s;
+	}
+	static String decodeMsg(String msgError) {
+		String s = "";
+		int i = 0;
+		while(i + 3 <= msgError.length()) {
+			String word = msgError.substring(i, i + 3);
+			s += String.valueOf(correction(word));
+			i += 3;
+		}
+		return s;
+	}
+	static char correction(String triplet) {
+		if (triplet.charAt(0) == triplet.charAt(1))
+			return triplet.charAt(0);
+		else if (triplet.charAt(0) == triplet.charAt(2))
+			return triplet.charAt(0);
+		else
+			return triplet.charAt(1);
 	}
 }
